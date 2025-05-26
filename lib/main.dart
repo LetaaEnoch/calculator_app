ae953e85ed87c122490784686983a13234f56fa3
LetaaEnoch <35469987+LetaaEnoch@users.noreply.github.com>
2025-05-26 12:13:23 +0300

    color

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,15 @@ void main() {
 class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Simple Calculator', home: CalculatorHome());
+    return MaterialApp(
+      title: 'Simple Calculator',
+      home: CalculatorHome(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
@@ -19,14 +27,53 @@ class CalculatorHome extends StatefulWidget {
 }
 
 class _CalculatorHomeState extends State<CalculatorHome> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Calculator')),
-      body: Center(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
+      appBar: AppBar(
+        title: Text(
+          'Calculator',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('$_counter', style: Theme.of(context).textTheme.displayLarge),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                  onPressed: _decrementCounter,
+                  child: Icon(Icons.remove),
+                ),
+                SizedBox(width: 20),
+                OutlinedButton(
+                  onPressed: _incrementCounter,
+                  child: Icon(Icons.add),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
